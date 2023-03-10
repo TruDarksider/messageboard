768bb4d310631e1e4363a592e988b85ef81a1415
Tyler Hopkin <tyler.hopkin12@gmail.com>
2023-03-10 15:43:37 -0600

    Added passport initialization logic

--- app.js
+++ app.js
@@ -1,36 +1,97 @@
-require('dotenv').config();
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const dotenv = require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bcrypt = require('bcrypt');
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var messageboardRouter = require('./routes/messageboard')
+
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const messageboardRouter = require('./routes/messageboard')
 
 var app = express();
 
 //Set up mongoose connection
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 mongoose.set('strictQuery', false);
 const mongoDB = process.env.MONGODB_URI;
 
 main().catch(err=>console.log(err));
 async function main(){
     await mongoose.connect(mongoDB);
-    console.log('Connected to MongoDB')
 }
+
+const User = mongoose.model(
+  'User',
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    isMember: { type: Boolean },
+    isAdmin: {type: Boolean}
+  })
+)
+
+//Passport Setup
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username })
+      .then(user => {
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        }
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (err) throw err
+          if (res) {
+            // passwords match! log user in
+            return done(null, user)
+          } else {
+            // passwords do not match!
+            return done(null, false, { message: "Incorrect password" })
+          }
+        })
+      })
+      .catch(err => {
+        return done(err);
+      }
+    )
+  })
+);
+
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    cb(null, { id: user.id, username: user.username });
+  });
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
