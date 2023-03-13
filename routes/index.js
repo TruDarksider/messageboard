a0d0ecc024c59ac15e7d5907b077c23b863b159e
Tyler Hopkin <tyler.hopkin12@gmail.com>
2023-03-13 16:44:33 -0500

    Added logic for user creation, sign in, and sign out

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const passport = require("passport");
 
 /* GET home page. */
 router.get('/', (req, res) => {
@@ -8,6 +11,35 @@ router.get('/', (req, res) => {
         error: null,// err,
         data: null//results
       });
-    });
+});
+   
+router.get('/signin', (req, res) => {
+  res.render('signin', { title: 'Sign into Message Board'});
+})
+
+router.post("/signin",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
+);
+
+router.get('/signup', (req, res) => {
+  res.render('signup', {title: 'Sign up for Message Board'});
+})
+
+router.post('/signup', async (req, res, next) => {
+  const user = new User({
+    username: req.body.username,
+    password: await bcrypt.hash(req.body.password, 10)
+  }).save().then(res.redirect('/')).catch(err => next(err));
+})
+
+router.get('/signout', (req, res, next) => {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
 
 module.exports = router;
--- routes/messageboard.js
+++ routes/messageboard.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Message = require('../models/message');
+const Message = require('../models/Message');
 
 //Display messages
 const messages = async (req, res, next) => {
