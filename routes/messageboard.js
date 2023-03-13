7b4f28da5793d9b4ebc4484b6c3333dfc0c2bf4d
Tyler Hopkin <tyler.hopkin12@gmail.com>
2023-03-13 14:19:47 -0500

    Messageboard now retrieves messages from mongoDB and is able to display them

--- routes/messageboard.js
+++ routes/messageboard.js
@@ -1,12 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Message = require('../models/message');
 
-/* GET message board messages. */
-router.get('/', function(req, res, next) {
-    res.render('messageboard', {
-      title: 'The Message Board'
-  })
-});
+//Display messages
+const messages = async (req, res, next) => {
+  try {
+    const list_messages = await Message.find()
+      .sort({ posted: 1 })
+      .populate('author');
+    res.render('messageboard', { title: 'The Message Board', messages: list_messages })
+  } catch (err) {
+    return next(err);
+  }
+};
+
+/* GET message board routes. */
+router.get('/', messages);
 
 router.get('/members', function(req, res, next) {
     res.render('messageboard', {
