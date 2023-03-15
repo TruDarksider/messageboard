b204806798c0c03e48558ae70d07472cdccbf040
Tyler Hopkin <tyler.hopkin12@gmail.com>
2023-03-15 16:15:34 -0500

    User list now displays alphabetically

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/User')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   res.render('users', {
-    title: "List of Contributors"
+    title: "List of Contributors",
+    user_list: await User.find({}).sort({username: 1})
   })
 });
 
